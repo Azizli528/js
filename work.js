a68b207e8c1a0ac9dd7b8b47da09d140b8ca7be5
Azizli528 <mhrrm661@gmail.com>
2026-06-24 11:52:54 +0400

    header dom ve css kodlari elave olundu

---
+++ work.js
@@ -0,0 +1,45 @@
+const header = document.getElementById("header");
+
+const basliq = document.createElement("h2");
+basliq.textContent = "⚽Furniro";
+header.append(basliq);
+
+
+const Home = document.createElement("p");
+Home.textContent = "Home";
+header.append(Home); 
+
+
+const Shop = document.createElement("p");
+Shop.textContent = "Shop";
+header.append(Shop); 
+
+
+const About = document.createElement("p");
+About.textContent = "About";
+header.append(About); 
+
+
+const Contact = document.createElement("p");
+Contact.textContent = "Contact";
+header.append(Contact); 
+
+
+const Profil = document.createElement("p");
+Profil.textContent = "👤";
+header.append(Profil);
+
+
+const axtaris = document.createElement("p");
+axtaris.textContent = "🔍";
+header.append(axtaris);
+
+
+const Heart = document.createElement("p");
+Heart.textContent = "❤️";
+header.append(Heart);
+
+
+const CartIcon = document.createElement("p");
+CartIcon.textContent = "🛒";
+header.append(CartIcon);
